3c2364df41c8f6b5ed6eb59e46803f18b7314912
talana tefo <talanatefo@gmail.com>
2025-03-21 09:34:53 +0200

    modified event listener

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ document.addEventListener("DOMContentLoaded", () =>
     }
     //Attach Event Listeners:
     addButton.addEventListener("click", addTask);
-    taskInput.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
+    taskInput.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
         addTask();
       }
     });
